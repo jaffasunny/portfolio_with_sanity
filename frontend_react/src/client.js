5019b02c6da068f6090a0568ae8b1bd5d391ab28
jaffasunny <jaffer.sunny125@gmail.com>
2022-04-13 04:03:54 +0500

    Project completed

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -3,13 +3,10 @@ import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
 	projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-	// projectId: "401mjdck",
 	dataset: "production",
 	apiVersion: "2022-02-01",
 	useCdn: true,
 	token: process.env.REACT_APP_SANITY_TOKEN,
-	// token:
-	// "skgYoUzciGUUZe1afnEGkwnv6NcoFKSwXQNli6sk3kQQ72N4tIYWQZlwPY3liu3NoaVFbFQM9x7DrAWjeV5cqBoCiiQ5XBDfgeymErnBCfXmDDpDpyGqAivXLHzGtmLUcDBKMUAySgQkGW4eRICGQBWxpPBA0Y9NKiO3DCspYoCyKeQsudkz",
 });
 
 const builder = imageUrlBuilder(client);
