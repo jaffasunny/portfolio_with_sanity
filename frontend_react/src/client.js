e7194b0f746afd890d172288f1b12af6a10a9373
jaffasunny <jaffer.sunny125@gmail.com>
2022-04-13 05:50:43 +0500

    Project completed

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -1,6 +1,13 @@
 import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
+// export const client = sanityClient({
+// 	projectId: env.REACT_APP_SANITY_PROJECT_ID,
+// 	dataset: "production",
+// 	apiVersion: "2022-02-01",
+// 	useCdn: true,
+// 	token: env.REACT_APP_SANITY_TOKEN,
+// });
 export const client = sanityClient({
 	projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
 	dataset: "production",
--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -5,17 +5,20 @@ import { FaLinkedinIn, FaGithub } from "react-icons/fa";
 const SocialMedia = () => {
 	return (
 		<div className="app__social">
-			<a href="https://twitter.com/bbjaffer" target="_blank">
+			<a href="https://twitter.com/bbjaffer" target="_blank" rel="noreferrer">
 				<div>
 					<BsTwitter />
 				</div>
 			</a>
-			<a href="https://www.linkedin.com/in/jaffer-sunny" target="_blank">
+			<a
+				href="https://www.linkedin.com/in/jaffer-sunny"
+				target="_blank"
+				rel="noreferrer">
 				<div>
 					<FaLinkedinIn />
 				</div>
 			</a>
-			<a href="https://github.com/jaffasunny" target="_blank">
+			<a href="https://github.com/jaffasunny" target="_blank" rel="noreferrer">
 				<div>
 					<FaGithub />
 				</div>
